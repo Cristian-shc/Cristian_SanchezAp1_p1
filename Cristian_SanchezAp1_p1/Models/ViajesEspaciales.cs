2cca405d5a569335b2be66a1bd307a17f9790aa1
cristian sanchez <crist@DESKTOP-DJBMKI7>
2026-02-02 21:07:41 -0400

    ultimo push

---
+++ Cristian_SanchezAp1_p1/Models/ViajesEspaciales.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Cristian_SanchezAp1_p1.Models;
+
+public class ViajesEspaciales
+{
+    [Key]
+    public int ViajeId { get; set; }
+    
+    public int fechaid { get; set; }
+    
+    public string Descripcion { get; set; }
+
+    public int costo { get; set; }
+    
+
+}
